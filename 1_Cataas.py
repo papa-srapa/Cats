f048f000c854e149a2101d22af3e7bfd575a2165
andreiandreev <flyinglotus026@gmail.com>
2024-11-05 14:24:59 +0500

    Создано окно и виджеты

--- 1_Cataas.py
+++ 1_Cataas.py
@@ -1,1 +1,24 @@
 from tkinter import *
+import requests  # отправляет HTTP запросы и получает ответ
+from PIL import Image, ImageTk  # Для работы с изображением
+from io import BytesIO  # Позволяет работать для ввода и вывода информации
+
+
+window = Tk()
+window.title('Cats')
+window.geometry('600X480')
+
+label = Label(text='')
+label.pack()
+
+
+url = 'https://cataas.com/cat'
+img = load.image(url)
+
+if img:  # Если переменная не пустая
+    label.config(image=img)  # То устанавливаем картинку на метку
+    label.image = img  # Что бы сборщик мусора не удалил картинку
+
+
+window.mainloop()
+
