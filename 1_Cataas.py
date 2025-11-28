27818c2be6343a8c052b4c9d717b42b1c4095213
Andrey Studensky <Gogo88.93@mail.ru>
2025-11-28 12:18:29 +0500

    Добавлено изменения размера изображения

--- 1_Cataas.py
+++ 1_Cataas.py
@@ -25,22 +25,53 @@ def set_image():  # Функция будет загружать новое из
         label.image = img  # Что бы сборщик мусора не удалил картинку
 
 
+def exit_1():
+    window.destroy()  # Завершаем работу программы
+
+
 window = Tk()
 window.title('Cats')
 window.geometry('600x520')
 
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label='Файл', menu=file_menu)
+file_menu.add_command(label='Загрузить фото', command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label='выход', command=exit_1)
+
 label = Label()
 label.pack()
 
-update_button = Button(text='Обновить', command=set_image)
-update_button.pack()
+
+# menu_bar = Menu(window)
+# window.config(menu=menu_bar)
+
+
+# file_menu = Menu(menu_bar, tearoff=0)
+# menu_bar.add_cascade(label='Файл', menu=file_menu)
+# file_menu.add_command(label='Загрузить фото', command=set_image)
+# file_menu.add_separator()
+# file_menu.add_command(label='выход', command=exit_1)
 
 
 
-url = 'https://cataas.com/cat'
+# file_menu = Menu(menu_bar, tearoff=0)
+# menu_bar.add_cascade(label='Файл', menu=file_menu)
+# file_menu.add_command(label='Загрузить фото', command=set_image)
+# file_menu.add_separator()
+# file_menu.add_command(label='выход', command=exit_1)
 
+# update_button = Button(text='Обновить', command=set_image)
+# update_button.pack()
+
+
+
+url = 'https://cataas.com/cat/cute'
 set_image()  # Вызываем функцию при запуске программы для появления первого изображения
-
 
 
 window.mainloop()
